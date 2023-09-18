130cf229c7570fa0190c4aaac10c1b887b175594
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-19 02:27:45 +0600

    even and odd use array

---
+++ arrayOddandEven.js
@@ -0,0 +1,38 @@
+//  const arr=[1,2,3,4,5,6,7,8,9,10,11];
+
+//  const odd= arr.filter(number => {
+//     return number%2 !== 0;
+
+//  });
+//  console.log(odd);
+
+
+
+
+ var even= [100,22,3,44,55,66,74,673,76];
+
+   var result = even.filter 
+   (number =>{
+            return number % 2 === 0;
+   });
+   console.log(result);
+
+
+//   is given arrays values 1 2 3 4 5 6 7 8 9 10 11 find you odd numbers
+   
+
+ var odd = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+ var number= odd.filter (number => {
+
+     return number % 2 !== 0;
+
+ });
+ console.log(number);
+
+//  even number array output
+ var arr = [32,44,55,52,53,22,53,5,66,3243,63,32534];
+   var  result = arr.filter( number => {
+   return number % 2 === 0;
+   });
+   console.log(result);
