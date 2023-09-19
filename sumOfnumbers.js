b2076fa0558205403f8d19a1d670a1fcaac1d34c
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-19 22:33:45 +0600

    multiplicationOfnumbers

---
+++ sumOfnumbers.js
@@ -0,0 +1,19 @@
+// 1-7 porjonto jog krte chie 
+//  1+2+3+4+5+6+7
+// let sum=0;
+//  for( var i=1; i<=7; i++){
+//     sum=sum+i;
+//     console.log(i,sum);
+//  }
+function sumOfnumber(number){
+    let sum =0;
+for (let i=1; i<=number; i++)
+{
+    result =sum+i;
+    console.log(i, result);
+   return sum;
+}
+const result = sumOfnumber(7);
+console.log(result);
+
+}
