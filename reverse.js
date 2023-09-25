bbea56523c19525ef1a2889b36b457235efd6539
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-25 23:15:49 +0600

    reverse string

---
+++ reverse.js
@@ -0,0 +1,11 @@
+function reverseString(text){
+    // for (let i=text.length; i>= 0;  i--) ulta reverse
+    for (let i =0; i<text.length ; i++)
+    {
+        const element =text[i];
+        console.log(element);
+
+    }
+}
+const myString='I am a bad boy';
+const reversed=reverseString(myString);
