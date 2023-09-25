fdcdc29adbab3c2cead6fa3c9ed168daa00f0bd8
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-25 19:58:32 +0600

    variable swap

---
+++ swap.js
@@ -0,0 +1,9 @@
+let first =6;
+let second =7;
+console.log(first,second);
+ aita thik ace out put 
+ const temp = first;
+ first=second;
+ second=temp;
+// [first, second]=[second, first];
+ console.log(first,second);
