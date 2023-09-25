ccc2c018fc106ae43b48c85bf19246d986ea61a3
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-25 20:37:10 +0600

    max elements

---
+++ max.js
@@ -0,0 +1,15 @@
+const jim =84;
+const dela =99;
+const chinku =77;
+if(dela<jim && jim>chinku){
+    console.log('jim win this cake bcz he highest marks in classroom');
+
+}
+else if(dela >jim && dela>chinku){
+    console.log('dela will   get the cake');
+}else{
+    console.log('chinku will get the cake');
+}
+// alternative
+
+    
--- swap.js
+++ swap.js
@@ -1,7 +1,7 @@
 let first =6;
 let second =7;
 console.log(first,second);
- aita thik ace out put 
+//  aita thik ace out put 
  const temp = first;
  first=second;
  second=temp;
