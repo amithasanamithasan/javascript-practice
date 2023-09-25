fa7c015f4b7bf17053913cfdee66288eee87850d
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-25 11:54:54 +0600

    string

---
+++ case.js
@@ -0,0 +1,22 @@
+ const userName='blackPink';
+ const Inputuser='blackPink';
+ console.log(userName.toLocaleLowerCase());
+ console.log(Inputuser.toLocaleUpperCase());
+ console.log(userName.toLocaleLowerCase());
+ if (userName.toLocaleLowerCase() === Inputuser.toLocaleLowerCase()){
+     console.log('Valid user name');
+
+ }else{
+     console.log('Invalid user name ');
+ }
+
+// const userName ='blackPink';
+// const Inputuser='blackPinK';
+
+// if(userName !== Inputuser)
+// {
+//     console.log('invalid user name ');
+
+// }else{
+//     console.log('valid user name ');
+// }
