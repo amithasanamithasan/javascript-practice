e377272962400ad21707660a6784b00fe6ef7142
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-25 19:44:11 +0600

    Math.randomfuncation

---
+++ math.js
@@ -0,0 +1,29 @@
+
+const result = Math.pow(3, 8);
+console.log(result);
+
+const num1=25;
+const num2=45;
+// differance 
+const gap =Math.abs(num1-num2);
+console.log(gap);
+
+if (gap<5){
+    console.log('you guys can be friend');
+
+}else{
+    console.log('you guys stay part');
+}
+// vognaosho
+const number=2.4999;
+const fullnumber =Math.round(number);
+// console.log(fullnumber);
+const result3=Math.ceil(2.00002);
+// console.log(result3);
+const result4 =Math.floor(456.259);
+console.log(result4);
+
+for(var i=0; i < 20; i++){
+    const random=Math.round(Math.random()*6);
+    console.log(random);
+}
