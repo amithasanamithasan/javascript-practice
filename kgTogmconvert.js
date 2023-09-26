0566daf20d26c54cd428b4b58720ec41b0734fc1
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-26 16:16:29 +0600

    kgtogram gram totaka

---
+++ kgTogmconvert.js
@@ -0,0 +1,20 @@
+// 1 kg dall er dam 800 taka hole 250gm er dam koto?
+ const dall=800;
+ const kg=1000;    //  we know 1kg =1000gm
+ const Grm=250;
+
+ const kilo=dall/kg;
+//  console.log(kilo);
+  const result = kilo*Grm;
+    console.log('250 gm er dam ase, taka',result);
+
+
+// 250 takie powa jie 1kg chall tahole 100 takie koto gram chall powa jabe ?
+  var chall=250 ;
+ var gram =1000; // we know 1000gm =1kg
+
+ var Result =gram/chall;
+ var taka =Result*100;
+  console.log('100 takie pawoa jie =',taka);
+ 
+
