f098e294a0216d1de1c37ba192dd01a92b519206
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-09-26 13:26:13 +0600

    reverse all function

--- reverse.js
+++ reverse.js
@@ -1,11 +1,43 @@
-function reverseString(text){
-    // for (let i=text.length; i>= 0;  i--) ulta reverse
-    for (let i =0; i<text.length ; i++)
-    {
-        const element =text[i];
-        console.log(element);
+   //   ulta reverse
+ function reverseString(text){
+  
+       for (let i=text.length-1; i>= 0;  i--) 
+      for (let i =0; i<text.length ; i++)
+     {
+         const element =text[i];
+         console.log(element);
 
+     }
+  }
+  const myString='I am a bad boy';
+
+  const reversed=reverseString(myString);
+
+//    ulta revased string nich thake start hobe
+ function ultarevarsed (input){
+     for(let i = input.length-1; i>=0; i--){
+         const elements=input[i];
+         console.log(elements);
+
+     }
+ }
+ const string='i have done this work';
+ const revarse=ultarevarsed(string);
+ console.log(revarse);
+
+//   one letter  + one letter +one letter = full word
+
+function oneletter(num){
+    let revased='';
+    // for (let i=num.length-1; i>=0; i--)
+    for(let i=0; i<num.length;i++)
+    {
+        const elem = num[i];
+        revased=revased+elem;
+        console.log(elem, revased);
     }
+    return revased;
 }
-const myString='I am a bad boy';
-const reversed=reverseString(myString);
+const mystring='i love in bangladesh';
+const revased=oneletter(mystring);
+console.log(revased);
